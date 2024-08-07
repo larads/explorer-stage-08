aff02dc7a36149c9e98a98a2d00617c92bd95db3
larads <dasilval.mariana@gmail.com>
2024-08-06 21:16:21 -0400

    ✨ feat: Query Params

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ const express = require('express')
 const app = express()
 
 app.get('/message/:id/:user', (request, response) => {
-    const { id, user } = request.params
+  const { id, user } = request.params
 
-    response.send(`
+  response.send(`
     Mensagem ID: ${id}.
     Para o usuário ${user}.  
   `)
+})
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`)
 })
 
 const PORT = 3333
