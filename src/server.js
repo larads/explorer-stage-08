4c9a771e35fc7b440d7e745d58f44eaa83a8408a
larads <dasilval.mariana@gmail.com>
2024-08-06 22:01:50 -0400

    ✨ feat: Conectando com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-require("express-async-errors")
-
-const AppError = require("./utils/AppError")
+require('express-async-errors')
+const database = require('./database/sqlite')
+const AppError = require('./utils/AppError')
 
 const express = require('express')
 
@@ -11,18 +11,21 @@ app.use(express.json())
 
 app.use(routes)
 
+database()
+
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
-            status: "error",
+            status: 'error',
             message: error.message
         })
     }
+
     console.error(error)
 
     return response.status(500).json({
-        status: "error",
-        message: "Internal Server Error"
+        status: 'error',
+        message: 'Internal server error'
     })
 })
 
